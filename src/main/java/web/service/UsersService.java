eeed92b8e9b1226b2db6596aa1da52930683e955
Pahanpretty <pifpaf96@mail.ru>
2024-10-31 17:06:03 +0300

    first commit

---
+++ src/main/java/web/service/UsersService.java
@@ -0,0 +1,47 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import web.model.User;
+import web.userDAO.UserDAO;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UsersService implements UserService{
+
+    private UserDAO userDAO;
+
+    @Autowired
+    public void setUserDao(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDAO.addUser(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    public void editUser(User user) {
+        userDAO.editUser(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDAO.getUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+}
